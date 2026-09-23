8b770184807fc2af7d2e4ba8829f84e40a7a16b3
Yuri Andri Gani <hyuricane@gmail.com>
2026-09-23 09:48:40 +0700

    fix: "SUPER + V" not working on omarchy plugin version of cmdpp TUI. declare window tag "+terminal" to enable

--- cmdpp-bindings.lua
+++ cmdpp-bindings.lua
@@ -8,6 +8,7 @@ local launch_cmd = "setsid uwsm-app -- xdg-terminal-exec --app-id=cmdpp --title=
 -- Floating window rules for Command++
 o.window("cmdpp", { float = true })
 o.window("cmdpp", { center = true })
+o.window("cmdpp", { tag = "+terminal" })
 -- o.window("cmdpp", { size = { 960, 600 } })
 
 -- Keybinding: SUPER + SHIFT + H
